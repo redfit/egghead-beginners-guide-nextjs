b0612cd0112fe6dc371d1c9bcb051c5a43d21363
redfit <daisuke.fuchiwaki@kaminashi.com>
2023-07-21 21:47:19 +0900

    Serve Optimized Images Using the Next.js Image Component

--- exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
+++ exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
@@ -1,11 +1,19 @@
 // ✍️ import the Image component from 'next/image'
+import Image from 'next/image'
 
 // ✍️ import "lazar.png" statically
+import lazar from '../lazar.png'
+
+const ICON_URL =
+  'https://ca.slack-edge.com/T3AD86FUL-U05FLFQLH6D-70815a0f5c02-512'
 
 const Home = () => {
-  return {
-    /* ✍️ add the lazar image here */
-  }
+  return (
+    <>
+      <Image src={lazar} alt="Lazar" />
+      <Image src={ICON_URL} alt="Lazar" width={550} height={550} />
+    </>
+  )
 }
 
 export default Home
