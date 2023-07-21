019b93889dc4e81d9e80558242fb89730ccf1448
redfit <daisuke.fuchiwaki@kaminashi.com>
2023-07-21 21:22:59 +0900

    Serve Static Assets in a Next.js Project

--- exercises/exercise-04--serve-static-assets/pages/index.tsx
+++ exercises/exercise-04--serve-static-assets/pages/index.tsx
@@ -3,7 +3,7 @@ const Home = () => {
     <div>
       <h2>Hello 👋</h2>
       <p>My name is Lazar Nikolov</p>
-      {/* ✍️ add the "lazar.png" image here */}
+      <img src="/lazar.png" alt="lazar" />
     </div>
   )
 }
