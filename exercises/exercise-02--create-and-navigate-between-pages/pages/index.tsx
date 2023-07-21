eee9b5224e72a81b144dd85d8bc58978666c9ce7
redfit <daisuke.fuchiwaki@kaminashi.com>
2023-07-21 20:57:31 +0900

    Create New Pages in Next.js and Navigate Them Using Link

--- exercises/exercise-02--create-and-navigate-between-pages/pages/index.tsx
+++ exercises/exercise-02--create-and-navigate-between-pages/pages/index.tsx
@@ -1,10 +1,12 @@
 // ✍️ import the Link component from 'next/link'
+import Link from 'next/link'
+
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to the Home page!</h1>
-      {/* ✍️ put a link to the About page */}
+      <Link href="about">About</Link>
     </div>
   )
 }
