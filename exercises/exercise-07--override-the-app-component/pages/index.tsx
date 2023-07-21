48f86ef83063fe91e28db4f3d66a01a45fe34f1e
redfit <daisuke.fuchiwaki@kaminashi.com>
2023-07-21 22:36:03 +0900

    Override the Default App Component in Next.js

--- exercises/exercise-07--override-the-app-component/pages/index.tsx
+++ exercises/exercise-07--override-the-app-component/pages/index.tsx
@@ -1,11 +1,10 @@
 // ✍️ create a type Props containing title as string
 
 // ✍️ obtain the title prop
-const Home = () => {
+const Home = ({ title }: { title: string }) => {
   return (
     <div>
-      <h1>Overriding the App component</h1>
-      {/* ✍️ render the title prop */}
+      <h1>{title}</h1>
     </div>
   )
 }
