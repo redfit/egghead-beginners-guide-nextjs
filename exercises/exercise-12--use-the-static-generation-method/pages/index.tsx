d1c2bd7429cb31ab609c3a18bab70062b879e968
redfit <daisuke.fuchiwaki@kaminashi.com>
2023-07-23 22:15:00 +0900

    Understand SSG, ISR, and SSR Renderring methods used in Next.js

--- exercises/exercise-12--use-the-static-generation-method/pages/index.tsx
+++ exercises/exercise-12--use-the-static-generation-method/pages/index.tsx
@@ -1,4 +1,5 @@
-// ✍️ import GetStaticProps from 'next'
+import type { GetStaticProps } from 'next'
+import Link from 'next/link'
 import { List, ListItem, Container, Heading } from '@chakra-ui/react'
 
 type Props = {
@@ -11,13 +12,21 @@ const Home = ({ genres }: Props) => {
       <Heading mb={4}>Genres:</Heading>
       <List>
         {genres.map((genre) => (
-          <ListItem key={genre}>{genre}</ListItem>
+          <ListItem key={genre}>
+            <Link href={`/${genre}`}>{genre}</Link>
+          </ListItem>
         ))}
       </List>
     </Container>
   )
 }
 
-// ✍️ export the getStaticProps method and return an array of genres as props
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
+  return {
+    props: {
+      genres: ['rock', 'country', 'pop']
+    }
+  }
+}
 
 export default Home
